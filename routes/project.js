0ba7fa63a3631d54e91959a6389e9f6e48d43711
Brett Parsekian <bparseki@ucsd.edu>
2014-02-20 18:56:13 -0800

    putting it together lab

--- routes/project.js
+++ routes/project.js
@@ -1,10 +1,13 @@
 var models = require('../models');
 
-exports.projectInfo = function(req, res) { 
+exports.projectInfo = function(req, res) {
   var projectID = req.params.id;
 
   // query for the specific project and
   // call the following callback
+  models.Project
+        .find({"_id": projectID})
+        .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -18,6 +21,22 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+  var newProject = new models.Project({
+      "title": form_data["project_title"],
+      "date": form_data["date"],
+      "summary": form_data["summary"],
+      "image": form_data["image_url"]
+  });
+
+  newProject.save(afterSaved);
+  res.send();
+
+  function afterSaved(err) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    }
+  }
 }
 
 exports.deleteProject = function(req, res) {
@@ -25,4 +44,17 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  models.Project
+        .find({"_id": projectID})
+        .remove()
+        .exec(afterRemoved);
+
+  res.send();
+
+  function afterRemoved(err) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    }
+  }
 }
